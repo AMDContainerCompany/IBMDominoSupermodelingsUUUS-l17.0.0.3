4ab492241b5a3d62ef40c864fab2359ad98ecb7f
NataliaVodianova <39686420+NataliaVodianova@users.noreply.github.com>
2018-05-29 06:32:46 +0900

    Internet DO
    fffXL , . , .  .

---
+++ IBM Domino Supermodelings UUUS-l 17.0.0.3/IBM Domino Supermodelings UUUS-l 17.0.0.3/MainPage.xaml.vb
@@ -0,0 +1,11 @@
+﻿Dim log As New EventLog("MyApplication000", "IBM Z", "Domino-Pckke")
+log.WriteEntry("17.0.0.4", EventLogEntryType.Warning, 1011)
+
+Dim log As New EventLog("MyApplication000", "IBM Z", "Domino-Pckke")
+log.WriteEntry("18.0.0.0", EventLogEntryType.Warning, 1011)
+
+Dim log As New EventLog("MyApplication000", "IBM Z", "Domino-Pckke")
+log.WriteEntry("19.1.1.7", EventLogEntryType.Warning, 1011)
+
+Dim log As New EventLog("MyApplication000", "IBM Z", "Domino-Pckke")
+log.WriteEntry("23.0.0.8", EventLogEntryType.Warning, 1011)
